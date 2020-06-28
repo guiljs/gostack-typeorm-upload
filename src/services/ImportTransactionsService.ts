4e6d237987dc89db01051600198437a6b4e1bc48
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-06-27 23:21:41 -0300

    Solved ImportTransactionsService

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -8,9 +8,9 @@ import CreateTransactionService from './CreateTransactionService';
 
 class ImportTransactionsService {
   async execute(fileName: string): Promise<Transaction[]> {
-    const csvFilePath = path.resolve(uploadConfig.directory, fileName);
-
-    const readCSVStream = fs.createReadStream(csvFilePath);
+    const readCSVStream = fs.createReadStream(
+      path.join(uploadConfig.directory, fileName),
+    );
 
     const parseStream = csvParse({
       from_line: 2,
@@ -19,26 +19,38 @@ class ImportTransactionsService {
     });
 
     const parseCSV = readCSVStream.pipe(parseStream);
-    const transactions = [];
-    parseCSV.on('data', async line => {
-      console.log(line);
-      const [title, type, value, category] = line;
-      const transaction = await new CreateTransactionService().execute({
-        title,
-        value,
-        type,
-        category,
-      });
-      console.log(transaction);
-      transactions.push(transaction);
+
+    const lines: string[] = [];
+
+    parseCSV.on('data', line => {
+      lines.push(line);
     });
 
     await new Promise(resolve => {
       parseCSV.on('end', resolve);
     });
 
-    console.log('TRansactions');
-    console.log(transactions);
+    const transactions: Transaction[] = [];
+
+    for (const line of lines) {
+      console.log('linha', line);
+
+      const [title, type, value, category] = line.toString().split(',');
+
+      const transaction = await new CreateTransactionService().execute({
+        title,
+        type: type as 'income' | 'outcome',
+        value: Number.parseInt(value),
+        category,
+      });
+      console.log('transaction', transaction);
+      transactions.push(transaction);
+    }
+
+    console.log('lines', lines);
+
+    console.log('transactions : ', transactions);
+    await fs.promises.unlink(path.join(uploadConfig.directory, fileName));
     return transactions;
   }
 }
